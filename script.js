87676b8f41cff49bb875ea09dbd761827a13e8d1
juanjolopezramirez <juanjoselopezramirez@gmail.com>
2025-01-14 19:09:16 -0500

    migueFixedtry

---
+++ script.js
@@ -0,0 +1,7 @@
+function toggleElements() {
+    const socialIcons = document.querySelector('.social-icons');
+    const buttons = document.querySelector('.cta-buttons');
+
+    socialIcons.style.display = 'flex';
+    buttons.style.display = 'flex';
+}
